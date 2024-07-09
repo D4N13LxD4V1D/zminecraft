ed2cf7dec56d11fd86459c69c61ac50dd449af10
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-07-09 23:21:27 +0800

    chore: Initialize ECS database connection in separate function

--- src/ecs/root.zig
+++ src/ecs/root.zig
@@ -6,16 +6,20 @@ db: ?*c.sqlite3 = null,
 pub fn init() @This() {
     var self: @This() = .{};
 
-    if (c.sqlite3_open(":memory:", &self.db) != c.SQLITE_OK) {
-        std.log.err("Failed to open database: {s}\n", .{c.sqlite3_errmsg(self.db)});
-        std.process.exit(1);
-    }
-    defer if (c.sqlite3_close(self.db) != c.SQLITE_OK)
-        std.log.err("Failed to close database: {s}\n", .{c.sqlite3_errmsg(self.db)});
+    self.initECS();
 
     return self;
 }
 
 pub fn run(self: @This()) !void {
     _ = self;
+}
+
+fn initECS(self: @This()) void {
+    if (c.sqlite3_open(":memory:", &self.db) != c.SQLITE_OK) {
+        std.log.err("Failed to open database: {s}\n", .{c.sqlite3_errmsg(self.db)});
+        std.process.exit(1);
+    }
+    defer if (c.sqlite3_close(self.db) != c.SQLITE_OK)
+        std.log.err("Failed to close database: {s}\n", .{c.sqlite3_errmsg(self.db)});
 }
